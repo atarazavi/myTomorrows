29649b290fb200413b082c2e627fc1d7d94312e5
Rezevoo <arazavi@identecsolutions.com>
2023-09-15 15:12:45 +0200

    basic layout and folder structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'trials',
+    pathMatch: 'full',
+  },
+  {
+    path: 'trials',
+    loadChildren: () => import('./features/trials/trials.module').then(m => m.TrialsModule),
+  },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
