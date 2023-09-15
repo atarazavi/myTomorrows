5a5042f8a7b9aca4910bb6e731cfa158cb8e1f51
Rezevoo <arazavi@identecsolutions.com>
2023-09-15 15:19:40 +0200

    Snack bugfix

--- src/app/shared/components/snack/snack.module.ts
+++ src/app/shared/components/snack/snack.module.ts
@@ -5,12 +5,8 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { SnackService } from './snack.service';
 import { SharedModule } from '#shared/shared.module';
 import { MatButtonModule } from '@angular/material/button';
-import { CustomizedSnackComponent } from './component/customized-snack.component';
 
 @NgModule({
-  declarations: [
-    CustomizedSnackComponent,
-  ],
   imports: [
     CommonModule,
     MatSnackBarModule,
