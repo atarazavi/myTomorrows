5a641cd364b191b08ae1d366f04d9d136f3b87dd
Rezevoo <arazavi@identecsolutions.com>
2023-09-16 01:57:23 +0200

    Implement each 5 second update

--- src/app/features/trials/store/trials.store.ts
+++ src/app/features/trials/store/trials.store.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ComponentStore } from '@ngrx/component-store';
-import { EMPTY, pipe } from 'rxjs';
-import { catchError, switchMap, tap } from 'rxjs/operators';
+import { EMPTY, interval, pipe } from 'rxjs';
+import { catchError, switchMap, take, tap } from 'rxjs/operators';
 import { HttpErrorResponse, HttpParams } from '@angular/common/http';
 
 import { Study } from '../models/trial.model';
@@ -19,7 +19,7 @@ export class TrialsStore extends ComponentStore<TrialsStoreInterface> {
   public studiesList: Study[] = [];
   readonly trials$ = this.select((state) => state.trials);
   readonly trialsError$ = this.select((state) => state.trialsError);
-  public buffer: Study[] = [];
+  private trialsBuffer: Study[] = [];
 
   constructor(
     private trialsService: TrialsService,
@@ -30,6 +30,67 @@ export class TrialsStore extends ComponentStore<TrialsStoreInterface> {
       trialsError: null,
       nextPageToken: null,
     });
+    // Initialize the timer and data flow
+    this.initTimer();
+  }
+  private initTimer() {
+    // Start a timer that ticks every 5000ms (5s)
+    interval(5000).subscribe(() => {
+      this.updateTrialsList();
+    });
+  }
+
+  private updateTrialsList() {
+    // If buffer is empty, fetch new trials
+    if (this.trialsBuffer.length === 0) {
+      this.fetchTrials(this.get().nextPageToken).pipe(take(1)).subscribe();
+      return;
+    }
+
+    // Get the next trial from the buffer
+    const nextTrial = this.trialsBuffer.shift();
+
+    // Get the current list of trials
+    const currentTrials = [...(this.get().trials || [])];
+
+    // Add the next trial to the list
+    if (nextTrial) {
+      currentTrials.push(nextTrial);
+    }
+
+    // If the list exceeds 10, remove the oldest (first) item
+    if (currentTrials.length > 10) {
+      currentTrials.shift();
+    }
+
+    // Update the state
+    this.patchState({ trials: currentTrials });
+  }
+
+  private fetchTrials(nextPageToken: string | null) {
+    return this.trialsService.get(0, this.createQueryParams(nextPageToken)).pipe(
+      tap({
+        next: (trials) => {
+          // Update buffer with new trials
+          this.trialsBuffer = trials.studies;
+
+          this.patchState({
+            nextPageToken: trials.nextPageToken,
+          });
+          this.resetErrors();
+        },
+        error: (trialsError) => this.patchState({ trialsError: trialsError }),
+      }),
+      catchError(() => EMPTY)
+    );
+  }
+
+  private createQueryParams(nextPageToken: string | null): HttpParams {
+    let params = new HttpParams();
+    if (nextPageToken) {
+      params = params.set('pageToken', nextPageToken);
+    }
+    return params;
   }
 
   loadTrials$ = this.effect<void>(
