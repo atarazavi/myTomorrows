eb4471f590a732147bcb6363f38eb4029f1d4073
Rezevoo <arazavi@identecsolutions.com>
2023-09-15 15:13:50 +0200

    Generic Backend Service

---
+++ src/app/shared/services/backend/generic-backend.service.ts
@@ -0,0 +1,60 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GenericBackendService<T, TId> {
+
+  constructor(@Inject(String) public url: string, public httpClient: HttpClient) { }
+
+  get(retryCount = 1) {
+    return this.httpClient
+      .get<T>(this.url)
+      .pipe(retry(retryCount), catchError(this.handleError));
+  }
+
+  getByID(id: TId, retryCount = 1) {
+    return this.httpClient
+      .get<T>(this.url + '/' + id)
+      .pipe(retry(retryCount), catchError(this.handleError));
+  }
+
+  post(id: TId, data?: T, retryCount = 1) {
+    return this.httpClient
+      .post<T>(this.url + '/' + id, data)
+      .pipe(retry(retryCount), catchError(this.handleError));
+  }
+
+  postWithoutId(data?: T, retryCount = 1) {
+    return this.httpClient
+      .post<T>(this.url, data)
+      .pipe(retry(retryCount), catchError(this.handleError));
+  }
+
+  put(id: TId, data: T, retryCount = 1) {
+    return this.httpClient
+      .put<T>(this.url + '/' + id, data)
+      .pipe(retry(retryCount), catchError(this.handleError));
+  }
+
+  update(id: TId, data: T, retryCount = 1) {
+    return this.httpClient
+      .patch<T>(this.url + '/' + id, data)
+      .pipe(retry(retryCount), catchError(this.handleError));
+  }
+
+  delete(id: TId, retryCount = 1) {
+    return this.httpClient
+      .delete<T>(this.url + '/' + id)
+      .pipe(retry(retryCount), catchError(this.handleError));
+  }
+
+  handleError(error: HttpErrorResponse) {
+    return throwError(() => {
+      return error;
+    });
+  }
+}
