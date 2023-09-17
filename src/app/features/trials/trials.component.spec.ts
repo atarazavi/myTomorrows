c76225fa841341d5db809742ae142542f6499a7c
Rezevoo <arazavi@identecsolutions.com>
2023-09-17 19:26:21 +0200

    test trials component

--- src/app/features/trials/trials.component.spec.ts
+++ src/app/features/trials/trials.component.spec.ts
@@ -1,18 +1,100 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FavoritesComponent } from './components/favorites/favorites.component';
+import { of } from 'rxjs';
+import { subscribeSpyTo } from '@hirez_io/observer-spy';
+
 import { TrialsStore } from './store/trials.store';
 import { TrialsModule } from './trials.module';
+import { ComponentPage } from '#shared/test/component-page';
+import { TrialsComponent } from './trials.component';
+import { CreateKeyboardEvent } from '#shared/test/keyboard-event-stub';
+import { DateType } from './models/dateType.model';
+import { Trials } from './models/trial.model';
+import { UnitTestingModule } from '#shared/test/unit-testing.module';
+import { FlatStudy } from './models/flatStudy.model';
 
-describe('FavoritesComponent', () => {
-  let component: FavoritesComponent;
-  let fixture: ComponentFixture<FavoritesComponent>;
+
+class TrialsComponentPage extends ComponentPage<TrialsComponent> {
+  FilterInput() {
+    return this.querySelector<HTMLElement>('#filterTrialsTableInput');
+  }
+  FavoritesButton() {
+    return this.querySelector<HTMLButtonElement>('.favoriteButton');
+  }
+}
+
+describe('TrialsComponent', () => {
+  let component: TrialsComponent;
+  let fixture: ComponentFixture<TrialsComponent>;
+  let page: TrialsComponentPage;
+  let testStore: TrialsStore;
+  const mockKeyboardEvent: KeyboardEvent = CreateKeyboardEvent('NCT123456');
+  const sampleTrialsMock: Trials = {
+    studies: [
+      {
+        protocolSection: {
+          identificationModule: {
+            nctId: "NCT123456",
+            organization: {
+              fullName: "Health Research Institute",
+            },
+            briefTitle: "Study on Cardiovascular Diseases",
+            officialTitle: "A Comprehensive Study on the Effects of X Drug on Cardiovascular Diseases",
+          },
+          statusModule: {
+            overallStatus: "Recruiting",
+            startDateStruct: {
+              date: new Date('2023-01-01T00:00:00'),
+              type: DateType.ACTUAL,
+            },
+          },
+        },
+      },
+      {
+        protocolSection: {
+          identificationModule: {
+            nctId: "NCT654321",
+            organization: {
+              fullName: "Mental Health Organization",
+            },
+            briefTitle: "Study on Anxiety Disorders",
+            officialTitle: "Investigating the Efficacy of Y Therapy for Anxiety Disorders",
+          },
+          statusModule: {
+            overallStatus: "Completed",
+            startDateStruct: {
+              date: new Date('2021-06-15T00:00:00'),
+              type: DateType.ESTIMATED,
+            },
+          },
+        },
+      },
+    ],
+    nextPageToken: "ABC123XYZ",
+  };
+  const flattenedTrialsMock: FlatStudy[] = [{
+    nctId: "NCT123456",
+    fullName: "Health Research Institute",
+    briefTitle: "Study on Cardiovascular Diseases",
+    officialTitle: "A Comprehensive Study on the Effects of X Drug on Cardiovascular Diseases",
+    overallStatus: "Recruiting",
+    startDate: new Date('2023-01-01T00:00:00'),
+    dateType: DateType.ACTUAL,
+  }, {
+    nctId: "NCT654321",
+    fullName: "Mental Health Organization",
+    briefTitle: "Study on Anxiety Disorders",
+    officialTitle: "Investigating the Efficacy of Y Therapy for Anxiety Disorders",
+    overallStatus: "Completed",
+    startDate: new Date('2021-06-15T00:00:00'),
+    dateType: DateType.ESTIMATED,
+  }];
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
-        FavoritesComponent,
+        TrialsComponent,
       ],
       providers: [
         TrialsStore,
@@ -20,15 +102,51 @@ describe('FavoritesComponent', () => {
       imports: [
         HttpClientTestingModule,
         TrialsModule,
+        UnitTestingModule,
         BrowserAnimationsModule
       ]
     });
-    fixture = TestBed.createComponent(FavoritesComponent);
+    testStore = TestBed.inject(TrialsStore);
+    fixture = TestBed.createComponent(TrialsComponent);
+    page = new TrialsComponentPage(fixture);
     component = fixture.componentInstance;
+    component.trials$ = of(sampleTrialsMock.studies);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create and load data', () => {
+    testStore.loadTrials$();
     expect(component).toBeTruthy();
+    component.loadData();
+    const observerSpyTrials = subscribeSpyTo(component.trials$);
+    const observerSpyTrialsResponse = observerSpyTrials.getLastValue();
+
+    if (observerSpyTrialsResponse) {
+      expect(component.dataSource.filteredData).toEqual(flattenedTrialsMock);
+    }
+  });
+
+  it('should filter data when something is written in filter input', () => {
+    expect(component.dataSource.filteredData.length).toBe(2);
+
+    component.applyFilter(mockKeyboardEvent);
+    fixture.detectChanges();
+
+    expect(component.dataSource.filteredData.length).toBe(1);
+  });
+
+  it('should call applyFilter when something is written in filter input', () => {
+    spyOn(component, 'applyFilter');
+    page.FilterInput().dispatchEvent(new Event('keyup'));
+    expect(component.applyFilter).toHaveBeenCalled();
+  });
+
+  it('should call onToggleFavorite() when user clicks on a fav button', () => {
+    spyOn(component, 'onToggleFavorite');
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      page.FavoritesButton().click();
+      expect(component.onToggleFavorite).toHaveBeenCalled();
+    })
   });
 });
