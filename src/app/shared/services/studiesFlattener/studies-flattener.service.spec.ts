009e0c79e69f011a8a1564b2c1458c8efffd70ef
Rezevoo <arazavi@identecsolutions.com>
2023-09-17 20:57:26 +0200

    test flattener service

--- src/app/shared/services/studiesFlattener/studies-flattener.service.spec.ts
+++ src/app/shared/services/studiesFlattener/studies-flattener.service.spec.ts
@@ -1,9 +1,69 @@
 import { TestBed } from '@angular/core/testing';
 
 import { StudiesFlattenerService } from './studies-flattener.service';
+import { DateType } from 'src/app/features/trials/models/dateType.model';
+import { Study } from 'src/app/features/trials/models/trial.model';
+import { FlatStudy } from 'src/app/features/trials/models/flatStudy.model';
 
 describe('StudiesFlattenerService', () => {
   let service: StudiesFlattenerService;
+
+  const sampleStudiesMock: Study[] = [{
+    protocolSection: {
+      identificationModule: {
+        nctId: "NCT123456",
+        organization: {
+          fullName: "Health Research Institute",
+        },
+        briefTitle: "Study on Cardiovascular Diseases",
+        officialTitle: "A Comprehensive Study on the Effects of X Drug on Cardiovascular Diseases",
+      },
+      statusModule: {
+        overallStatus: "Recruiting",
+        startDateStruct: {
+          date: new Date('2023-01-01T00:00:00'),
+          type: DateType.ACTUAL,
+        },
+      },
+    },
+  },
+  {
+    protocolSection: {
+      identificationModule: {
+        nctId: "NCT654321",
+        organization: {
+          fullName: "Mental Health Organization",
+        },
+        briefTitle: "Study on Anxiety Disorders",
+        officialTitle: "Investigating the Efficacy of Y Therapy for Anxiety Disorders",
+      },
+      statusModule: {
+        overallStatus: "Completed",
+        startDateStruct: {
+          date: new Date('2021-06-15T00:00:00'),
+          type: DateType.ESTIMATED,
+        },
+      },
+    },
+  },
+  ];
+  const flattenedTrialsMock: FlatStudy[] = [{
+    nctId: "NCT123456",
+    fullName: "Health Research Institute",
+    briefTitle: "Study on Cardiovascular Diseases",
+    officialTitle: "A Comprehensive Study on the Effects of X Drug on Cardiovascular Diseases",
+    overallStatus: "Recruiting",
+    startDate: new Date('2023-01-01T00:00:00'),
+    dateType: DateType.ACTUAL,
+  }, {
+    nctId: "NCT654321",
+    fullName: "Mental Health Organization",
+    briefTitle: "Study on Anxiety Disorders",
+    officialTitle: "Investigating the Efficacy of Y Therapy for Anxiety Disorders",
+    overallStatus: "Completed",
+    startDate: new Date('2021-06-15T00:00:00'),
+    dateType: DateType.ESTIMATED,
+  }];
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
@@ -12,5 +72,10 @@ describe('StudiesFlattenerService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should flatten trial object', () => {
+    const returnedArray = service.flattenStudies(sampleStudiesMock);
+    expect(returnedArray).toEqual(flattenedTrialsMock);
   });
 });
